b1de3dc0e12ff193d6e5d3fc16b4a998930a7af9
Ralph-Wang <ralph_ld@aliyun.com>
2014-12-15 20:30:05 +0800

    prepare v0.4.0

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup, find_packages
 
-version = '0.3.2'
+version = '0.4.0'
 
 setup(name='should',
       version=version,
       description="assert with should",
       long_description="""
       不太会 RST, 详情及源码见 https://github.com/Ralph-Wang/should
+
+      from should import it
+
+      it(1).should.be.int.also.be.equal(1)
+
+      it(lambda: int('abc')).should.throw(ValueError)
       """,
       keywords='assert,should,test,BDD',
       author='Ralph-Wang',
