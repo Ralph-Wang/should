989d6e4784464fd6d9567b4ada0b0e7312c792e8
Ralph-Wang <ralph_ld@aliyun.com>
2014-12-31 18:10:08 +0800

    fuck 3.* metaclass

--- should/ext/chains.py
+++ should/ext/chains.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 
 
-class _ChainMeta(object):
-
-    def __new__(cls, name, bases, attrs):
-        chain = property(lambda self: self)
-        names = ['should', 'have', 'an', 'of', 'a', 'be', 'also', 'which']
-        attrs.update({}.fromkeys(names, chain))
-        return type(name, bases, attrs)
+def chain_meta(name, bases, attrs):
+    chain = property(lambda self: self)
+    names = ['should', 'have', 'an', 'of', 'a', 'be', 'also', 'which']
+    attrs.update({}.fromkeys(names, chain))
+    return type(name, bases, attrs)
 
 
-class Chain(object):
-    __metaclass__ = _ChainMeta
+Chain = chain_meta('Chain', (object,), {})
